ed3243dca9faa3f3dc5e7f9c29e3f1c578dd84ad
MuhammadSaeedSaad <mouhammedsaed@gmail.com>
2021-01-30 00:22:19 +0200

    making /projects dir then making the /mini_projects dir the putting the first mini-project inside of it which is (the diastic machine)

---
+++ projects/mini_projects/diastic_machine/diastic/sketch.js
@@ -0,0 +1,37 @@
+let srctxt;
+let words;
+let phrase = "";
+let currentWord = 0;
+
+function diastic(seed, words){
+  for (var i=0; i < seed.length; i++){
+    var c = seed.charAt(i);
+    for (var j=currentWord; j < words.length; j++){
+      if (words[j].charAt(i) === c){
+        phrase += words[j] + " ";
+        currentWord += 1;
+        break;
+      }
+    }
+  }
+  return phrase;
+}
+
+// loading the source text to search in
+function preload(){
+  srctxt = loadStrings("abc.txt");
+}
+
+function setup() {
+  noCanvas();
+  srctxt = join(srctxt, " ");
+  words = splitTokens(srctxt, " ,.?!\"()")
+  // selecting the input box and the button by P5.js standard library functions
+  var seed = select("#seed");
+  var submit = select("#submit");
+  // handling the clicking on the button event using a javascript anonymous function
+  submit.mousePressed(function (){
+    let phrase = diastic(seed.value(), words);
+    createP(phrase);
+  });
+}
